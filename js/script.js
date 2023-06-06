5c721faf6b56d9385c62473f641660450a60f614
havr1985 <gavrtel1985@gmail.com>
2023-06-06 19:16:13 +0300

    section recipes

--- js/script.js
+++ js/script.js
@@ -1,9 +1,24 @@
-const swiper = new Swiper('.swiper', {
+ new Swiper('.swiper', {
   loop: true,
 
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
     },
+
+});
+
+new Swiper('.swiper-gallery', {
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  },
 
 });
